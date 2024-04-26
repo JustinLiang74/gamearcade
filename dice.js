30aa48a834f910976ad2a02f2200d5c152bf5ec4
Justin <justinliang74@gmail.com>
2024-04-26 15:39:42 -0700

    added dice rules

--- dice.js
+++ dice.js
@@ -1,9 +1,11 @@
 let total = 0;
+let die = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('rollButton').addEventListener('click', roll); 
   document.getElementById('resetButton').addEventListener('click', () => {
     total = 0;
+    document.getElementById('rollButton').disabled = false;
     document.getElementById('diceTotal').innerHTML = "";
     document.getElementById('diceResult').innerHTML = "";
     document.getElementById('diceRoll').innerHTML = "";
@@ -18,12 +20,14 @@ function roll() {
   check();
 }
 
-function check () {
-  if (total >= 22) {
+function check() {
+  if (total > 17 && total < 20) {
+    document.getElementById('diceResult').innerHTML = "You Win!";
+    document.getElementById('rollButton').disabled = true;
+  } else if (total >= 20) {
     document.getElementById('diceResult').innerHTML = "You Lose!";
-  } 
-  else if (total < 17)
-  {
+    document.getElementById('rollButton').disabled = true;
+  } else if (total < 17) {
     document.getElementById('diceResult').innerHTML = "Roll again!";
   }
 }
