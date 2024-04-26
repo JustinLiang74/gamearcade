acfc3e460ae46f98952e97716dbf891d6a945247
Justin <justinliang74@gmail.com>
2024-04-25 21:10:36 -0700

    Added dice rolling

---
+++ dice.js
@@ -0,0 +1,10 @@
+let die = Math.floor(Math.random() * 6) + 1;
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('rollButton').addEventListener('click', roll); 
+});
+
+function roll() {
+  die = Math.floor(Math.random() * 6) + 1;
+  document.getElementById('diceRoll').innerHTML = die;
+}
