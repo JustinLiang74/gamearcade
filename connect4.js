071c49699d185b4bf51679494b55c4aab257ca06
nikolaj pedersen <nikolajpedersen@campus-118-088.ucdavis.edu>
2024-04-29 16:25:37 -0700

    move handling implemented

--- connect4.js
+++ connect4.js
@@ -23,18 +23,24 @@ function renderBoard() {
     board = [];
     colHeights  = [5, 5, 5, 5, 5, 5, 5];
 
+    // nested for loops will iterate over each cell in the 6 x 7 board
     for (let row = 0; row < ROWS; row++) {
         let currentRow = [];
         for (let col = 0; col < COLS; col++) {
-            // JS
+            // add empty cell to row 
             currentRow.push(' ');
-            // HTML
+
+            // id of cell will be concatenation of it's row and col ["row,col", ...]
+            // this way we can identify each cell and handle moves/coloring them in
             let cell = document.createElement("div");
-            cell.id = row.toString() + "-" + col.toString();
+            cell.id = row.toString() + "," + col.toString();
+
+            // allows us to style the cell by the cell class
             cell.classList.add("cell");
             cell.addEventListener("click", handleMove);
             document.getElementById("board").append(cell);
         }
+        // builds the board row by row
         board.push(currentRow);
     }
 }
@@ -43,7 +49,37 @@ function renderBoard() {
 
 // drop piece into column on click, and alternate turns
 function handleMove(){
-    return
+    if (gameOver) {
+        return;
+    }
+
+    // cell clicked will be of form "row,col" from 2D board array
+    let cellClicked = this.id.split(",");
+    let col = parseInt(cellClicked[1])
+
+    // the row to place the piece depends on current height of that column
+    // column is full, if row (space left in col ) < 0
+    let row = colHeights[col];
+    if (row < 0) {
+        return;
+    }
+
+    // after getting the valid location for the move we need to update the board
+    let cell = document.getElementById(row.toString() + "," + col.toString());
+    if (currentPlayer == playerYellow) {
+        // populate the empty cell with CSS
+        // player alternates every click
+        currentPlayer = playerRed;
+        cell.classList.add("yellow-piece");
+    }
+    else {
+        currentPlayer = playerYellow;
+        cell.classList.add("red-piece")
+    }
+    
+    colHeights[col] -= 1;
+
+    checkWinner();
 }
 
 // use sliding window to check horizontally, vertically, and both diagonals for 4 in a row
